e1e7f02261e58a749e4ebc6db0f0bab8ccf0c622
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-29 17:29:06 +0545

    add hash password crypto

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { createHmac, randomBytes } = require("node:crypto");
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
@@ -32,6 +33,22 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+
+  next();
+});
 
 const User = model("user", userSchema);
 module.exports = User;
