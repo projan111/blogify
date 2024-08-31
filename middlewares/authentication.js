05ffecb92156307c8e6e2df04e25ecb469cfd0ce
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-31 17:57:33 +0545

    add user to navbar

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -5,7 +5,7 @@ function checkForAuthenticationCookie(cookieName) {
     const tokenCookieValue = req.cookies[cookieName];
 
     if (!tokenCookieValue) {
-      next();
+      return next();
     }
 
     try {
@@ -14,7 +14,7 @@ function checkForAuthenticationCookie(cookieName) {
     } catch (error) {
       console.log("error is in middleware", error);
     }
-    next();
+    return next();
   };
 }
 
