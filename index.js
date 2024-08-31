0189324853b8b06d3ef941dc8b77d1089bc2f1fb
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-31 16:53:24 +0545

    add cookie parser

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const path = require("path");
 const PORT = 4000;
 const ejs = require("ejs");
 const userRoutes = require("./routes/user");
+const cookieParser = require("cookie-parser");
 const { mongoConnection } = require("./config/db");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+// Mongodb Connection
 mongoConnection("mongodb://127.0.0.1:27017/blogify");
 
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
+// Set Template Engine
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// Home route
 app.get("/", (req, res) => {
   return res.render("home");
 });
---
+++ middlewares/authentication.js
@@ -0,0 +1,18 @@
+const { validateToken } = require("../services/authentication");
+
+function checkForAuthenticationCookie(cookieName) {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      next();
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
+    next();
+  };
+}
+
+module.exports = { checkForAuthenticationCookie };
