7d95aa1fb48df667de22c146efee4a079efe3c07
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-08-31 18:33:19 +0545

    add blog post route

--- routes/blog.js
+++ routes/blog.js
@@ -6,4 +6,9 @@ blogRoute.get("/add-new-blog", (req, res) => {
   return res.render("addBlog", { user: req.user });
 });
 
+blogRoute.post("/", (req, res) => {
+  console.log(req.body);
+  return res.redirect("/");
+});
+
 module.exports = blogRoute;
